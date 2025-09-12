e60c9e03b52eb40c091cca8a61a219414f71adfd
rolando <castillor493@gmail.com>
2025-09-11 22:23:10 -0600

    init

---
+++ src/com/itquetzali/news/models/NewsArticle.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import datetime
+
+class NewsArticle(BaseModel):
+    title:str
+    content:str
+    sumary:Optional[str] = None
+    source: str
+    author: Optional[str] = None
+    url: str
+    published_at: datetime
+    category: Optional[str] = None
+    language: str
+    tags: List[str] = Field(default_factory=list)
